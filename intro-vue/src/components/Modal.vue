3d2e63b4bc8e45f49d10b67d6249ca4637c464fc
Natalie <natalie.todd15@gmail.com>
2018-08-13 14:38:08 -0600

    login form has placeholders and prevent default is working.

--- intro-vue/src/components/Modal.vue
+++ intro-vue/src/components/Modal.vue
@@ -1,15 +1,21 @@
 <template>
 <div>
-<p>Test test. This is modal</p>
+<h1>Welcome to Travel Bug</h1>
+        <form @submit.prevent='searchForCredentials()'>
+            <!-- <label for='login'>Login</label> -->
+            <input v-model='userName' placeholder='Username' type='text' name='userName' id='userName' value=''>
+            <input v-model='passWord' placeholder='Password' type='text' name='passWord' id='passWord' value=''>
+            <button type='submit' name='button'>Login</button>
+        </form>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'Modal',
+  name: "Modal",
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: "Welcome to Your Vue.js App"
     };
   }
 };
--- intro-vue/src/router/index.js
+++ intro-vue/src/router/index.js
@@ -2,21 +2,9 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Modal from '@/components/Modal';
 import Footer from '@/components/Footer';
-// import HelloWorld from '@/components/HelloWorld';
-// import Footer from '@/components/Footer';
-// import Modal from '@/components/Modal';
 
 Vue.use(Router);
 
-// export default new Router({
-//   routes: [
-//     {
-//       path: '/',
-//       name: 'HelloWorld',
-//       component: HelloWorld,
-//     },
-//   ],
-// });
 export default new Router({
   routes: [
     {
