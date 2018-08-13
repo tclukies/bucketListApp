988c49b86e92b5aa53b3dd814eaf46a58d03c131
Natalie <natalie.todd15@gmail.com>
2018-08-13 13:42:51 -0600

    added a footer component and it is working

---
+++ intro-vue/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <p>Test test. I like cats. </p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+ 
+<style>
+</style>
--- intro-vue/src/router/index.js
+++ intro-vue/src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+// import HelloWorld from '@/components/HelloWorld';
+import Footer from '@/components/Footer';
 
 Vue.use(Router);
 
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'HelloWorld',
+//       component: HelloWorld,
+//     },
+//   ],
+// });
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'Footer',
+      component: Footer,
     },
   ],
 });
