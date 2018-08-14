4b5a1427c537931786f2bbf7344af50430bb00bf
Natalie <natalie.todd15@gmail.com>
2018-08-14 11:13:28 -0600

    added toggle function to signup button

--- intro-vue/src/components/Modal.vue
+++ intro-vue/src/components/Modal.vue
@@ -1,13 +1,16 @@
 <template>
 <div>
 <h1>Welcome to Travel Bug</h1>
+<div id='sigin'>
         <form @submit.prevent='searchForCredentials()'>
             <input placeholder='Username' type='text' name='username' id='username' value=''>
             <input placeholder='Password' type='text' name='password' id='password' value=''>
             <button type='submit' name='button'>Login</button>
             <label for='login'>New to Travel Bug?</label>
-            <button type='submit' name='button'>Sign up now!</button>
+            <button v-on:click='seen =! seen' type='submit' name='button'>Sign up now!</button>
         </form>
+        </div>
+        <div v-if='seen' id='registrationForm'>
         <form @submit.prevent='sendCredentials()'>
             <input placeholder='First Name' type='text' name='firstName' id='firstName' value=''>
             <input placeholder='Last Name' type='text' name='lastName' id='lastName' value=''>
@@ -17,15 +20,16 @@
             <input placeholder='Confirm Password' type='text' name='confirmPassword' id='confirmPassword' value=''>
             <button type='submit' name='button'>Create Profile</button>
         </form>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'Modal',
+  name: "Modal",
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      seen: false,
     };
   }
 };
