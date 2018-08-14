c67b3464e35bb5da39b200e0f53664ce9f143fa0
Natalie <natalie.todd15@gmail.com>
2018-08-13 23:29:06 -0600

    changed a href to router-link, everything seems to be working right. next need to link to Main page

--- intro-vue/src/router/index.js
+++ intro-vue/src/router/index.js
@@ -1,24 +1,19 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Front from '../views/Front';
-// import AddCountry from '../components/AddCountry';
-// import MapConfig from '@/components/MapConfig';
-import Main from '@/views/Main';
+import Main from '../views/Main';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
+  base: __dirname,
   routes: [
     {
       path: '/',
       name: 'Front',
       component: Front,
     },
-    // {
-    //   path: '/',
-    //   name: 'AddCountry',
-    //   component: AddCountry,
-    // },
     {
       path: '/',
       name: 'Main',
