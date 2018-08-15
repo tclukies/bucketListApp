9847aee6a80eb8d39a4e31ae5c2d90b6ce6e3f64
Ryan McCrory <ryanmccrory83@gmail.com>
2018-08-15 09:41:47 -0600

    added header and footer to FAQ page

---
+++ intro-vue/src/views/FAQ.vue
@@ -0,0 +1,25 @@
+<template>
+<div id='faq'>
+    <router-view></router-view>
+    <Header />
+    <Footer />
+    
+
+    </div>
+    
+</template>
+
+<script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+export default {
+  name: "FAQ",
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
+
+<style>
+</style>
