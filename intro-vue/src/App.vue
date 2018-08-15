ebc06dfea903bf908fcfaa8fcb480852c472089e
Natalie <natalie.todd15@gmail.com>
2018-08-15 10:12:16 -0600

    trying to fetch, getting cors error

--- intro-vue/src/App.vue
+++ intro-vue/src/App.vue
@@ -6,15 +6,24 @@
 </template>
 
 <script>
-
+const API_URL = "https://travel-bug-backend.herokuapp.com/profiles";
 export default {
-
+  data() {
+    return {
+      credentials: null
+    };
+  },
+  mounted() {
+    this.axios.get(API_URL).then(response => {
+      console.log(response.data);
+    });
+  }
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- intro-vue/src/main.js
+++ intro-vue/src/main.js
@@ -3,10 +3,16 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 // import Header from './components/Header';
 import Footer from './components/Footer';
 
+Vue.use(VueAxios, axios)
+
 Vue.config.productionTip = false;
+
+
 
 /* eslint-disable no-new */
 new Vue({
