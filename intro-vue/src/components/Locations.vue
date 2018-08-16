46495ba5052ba5729b463380f677eae6eb936b9e
Natalie <natalie.todd15@gmail.com>
2018-08-16 11:27:34 -0600

    added template formatting into location.vue file

--- intro-vue/src/components/Locations.vue
+++ intro-vue/src/components/Locations.vue
@@ -1,19 +1,51 @@
 <template>
-  <div>
-    <router-link to='/' tag='button'>Logout</router-link>
+  <div class="destination">
+    <ul>
+        <li v-for="post in posts" :key="post">
+            <h3 class="destination-h3">Country: {{ post.country_name }}</h3>
+            <h4 class="destination-activities">Activity: {{ post.activities }}</h4>
+        </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Header',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      name: "Locations",
+      msg: "Welcome to Your Vue.js App",
+      locationUrl: "https://travel-bug-backend.herokuapp.com/posts/profile/1",
+      locationData: null
     };
-  }
+  },
+  mounted() {
+    fetch(this.locationUrl, {
+      method: "get",
+      mode: "cors",
+      //   credentials: "same-origin",
+      headers: new Headers({ "Content-Type": "application/json" })
+    })
+      .then(resp => resp.json())
+      .then(resp => {
+        this.profileData = resp;
+        console.log(resp);
+        // console.log(this.locationData.profile);
+      });
+  },
+//   methods: {
+//       locationList(){
+//           for(let i = 0; i < this.profileData.posts[i].length; i++){
+
+//           }
+//       }
+//   }
 };
 </script>
  
 <style>
+.destination {
+  padding: 8px;
+  border: 1px solid red;
+}
 </style>
