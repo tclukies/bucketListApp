64d4d6b85f110624b04b12e3171430185f91687c
Thomas Clukies <Thomas@Thomass-MacBook-Pro.local>
2018-08-16 14:41:06 -0600

    functional with adding country delay for post fetch

--- intro-vue/src/components/AddCountry.vue
+++ intro-vue/src/components/AddCountry.vue
@@ -14,7 +14,7 @@
           <div>
             <textarea  v-model='form.visited' placeholder='True or False' type='text' name='visited' id='visited' value=''></textarea>
           </div>
-            <button type='submit' name='button'>Add Now!</button>
+            <button type='submit' name='button' onClick="setTimeout('history.go(0);',1000)">Add Now!</button>
             
         </form>
     </div>
